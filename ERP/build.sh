7f900216f2d78edec78f0b4db231670821e8b123
atarte <tarte.antoine@gmail.com>
2021-10-25 21:03:50 +0200

    update build.sh

--- ERP/build.sh
+++ ERP/build.sh
@@ -3,8 +3,7 @@
 # var
 Port=8000
 Time=$(date +%s)
-Image="abstergo-erp"
-Volume="Abstergo-ERP"
+Image="httpd"
 Name="Abstergo-ERP-${Time}"
 
 # Close the current container
@@ -14,31 +13,16 @@ ID=$(\
 	awk '{print $1}'\
 )
 
-if [ ! -z œ${ID} ]; then
+if [ ! -z ${ID} ]; then
 	echo "Closing container ${ID}"
 	docker container stop ${ID}
 fi
-
-# Deleting the last image / Creating a new image
-Repository=$(\
-	docker image ls --format="{{.Repository}}" |\
-	grep ${Image} |\
-	awk '{print $1}'\
-)
-
-if [ ! -z ${Repository} ]; then
-	echo "Removing image ${Image}"
-	docker image rm ${Image}
-fi
-
-echo "Creating a new image ${Image}"
-docker image build -t ${Image} .
 
 # Start a new container
 echo "Running new container ${Name}"
 docker container run -d -it \
 	-p ${Port}:80 \
-	-v $(pwd):/var/www/html/ \
+	-v $(pwd):/usr/local/apache2/htdocs/ \
 	--name=${Name} \
 	${Image}
 
