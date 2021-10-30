30081793e240c9db7605d3c45c885e6bc3399c81
atarte <tarte.antoine@gmail.com>
2021-10-30 22:10:19 +0200

    recreate wiki

---
+++ Wiki/build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# var
+Port=8080
+Time=$(date +%s)
+Image="httpd"
+Name="Abstergo-WIKI-${Time}"
+
+# Close the current container
+ID=$(\
+	docker container ls --format="{{.ID}}\t{{.Ports}}" |\
+	grep ":${Port}->" |\
+	awk '{print $1}'\
+)
+
+if [ ! -z ${ID} ]; then
+	echo "Closing container ${ID}"
+	docker container stop ${ID}
+fi
+
+# Start a new container
+echo "Running new container ${Name}"
+docker container run -d -it \
+	-p ${Port}:80 \
+	-v $(pwd):/usr/local/apache2/htdocs/ \
+	--name=${Name} \
+	${Image}
+
+echo "Container ${Name} is now running on port ${Port}"
