8d41030db5bad5337f5d75eeb93b1e261e9cee8e
atarte <tarte.antoine@gmail.com>
2021-10-31 14:07:32 +0100

    Script de backup pour linux

---
+++ Scripts/backup-linux/backup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Path
+backup_files="/home /var/spool/mail /etc /root /boot /opt"
+dest="/backup"
+
+# Name
+day=$(date +%A)
+archive_file="Debain-WEB-${day}.tgz"
+
+# backup
+echo "Start backup ${backup_files} to ${dest}/${archive_file}"
+
+tar czf ${dest}/${archive_file} ${backup_files}
+
+echo "Backup finished"
+
+ls -lh ${dest}
