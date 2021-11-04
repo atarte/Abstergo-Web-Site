edeb0afe4589fe592d55ebc9962cf37a5f785712
Lau-cesi <73819470+Lau-cesi@users.noreply.github.com>
2021-11-04 18:20:22 +0100

    Add files via upload

--- Scripts/Menu Powershell/menu powershell.ps1
+++ Scripts/Menu Powershell/menu powershell.ps1
@@ -1,8 +1,11 @@
-Write-host "Taper [1]- Get-ADUser"
+﻿Write-host "==========================MENU=========================" 
+Write-host "Taper [1]- Get-ADUser" 
 Write-host "Taper [2]- Get-ADGroup"
 Write-host "Taper [3]- Get-ADGroupMember"
 Write-host "Taper [4]- Get-ADOrganizationalUnit"
-
+Write-host "Taper [5]- Remove-ADUser"
+Write-host "Taper [6]- Remove-ADGroup"
+Write-host "=======================================================" 
 
 $Serveur = Read-Host "Choisissez un numero parmi les 4 options"
 
@@ -12,52 +15,79 @@ Switch ($Serveur)
     2{$Serveur="Get-ADGroup"}
     3{$Serveur="Get-ADGroupMember"}
     4{$Serveur="Get-ADOu"}
+    5{$Serveur="Remove-ADUser"}
+    6{$Serveur="Remove-ADGroup"}
     default{$Serveur = return}
 }
 
 If ($Serveur -eq "Get-ADUser")
 {
-   $User = Read-host "Veuillez renseigner le pseudo de l'utilisateur"
-   Get-ADUser -Identity $User |out-file -FilePath "c:\User.txt"
+
+   Write-host "Taper [1]- Recherche par pseudo"
+   Write-host "Taper [2]- Obtenir la liste de tous les users"
+    $User = Read-Host "Choisissez un numero parmi les 2 propositions"
+
+        Switch ($User)
+            {
+                1{$User="Pseudo"}
+                2{$User="Liste"}
+                default{$User = return}
+            }
+            If ($User -eq "Pseudo")
+                {
+                    $User = Read-host "Veuillez renseigner le pseudo de l'utilisateur" -confirm
+                    #Get-ADUser -Identity $User |out-file -FilePath "c:\User.txt"
+                    get-aduser  -Identity $User | Export-Csv c:\user.csv
+            }
+            else 
+            {
+                    
+                    get-aduser -filter *| Export-Csv "c:\ListeUser.csv"
+            }
 }
 
 
 elseif ($Serveur -eq "Get-ADGroup") 
 {
-    #$Groupe = Read-host "Groupe : "
-    #Get-ADGroup -Identity $Groupe |out-file -FilePath "c:\Groupe.txt"
+
     Write-host "Taper [1]- Recherche par Nom"
     Write-host "Taper [2]- Obtenir le SID"
-    $Group = Read-Host "Choisissez un numero parmi les 2 propositions"
+    Write-host "Taper [3]- Obtenir lea liste de tous les groupes"
+    $Group = Read-Host "Choisissez un numero parmi les 3 propositions"
 
         Switch ($Group)
             {
                 1{$Group="Nom"}
                 2{$Group="SID"}
+                3{$Group="Liste"}
                 default{$Group = return}
             }
             If ($Group -eq "Nom")
                 {
                     $Group = Read-host "Nom"
-                    Get-ADGroup -Identity $Group|out-file -FilePath "c:\Groupe.txt"
+                    Get-ADGroup -Identity $Group| Export-Csv "c:\Groupe.csv"
             }
-            else 
+            elseif ($Group -eq "SID") 
             {
                     $Group = Read-host "Nom"
-                    GET-Adgroup -Identity $Group|Select SID |out-file -FilePath "c:\SID.txt"
+                    GET-Adgroup -Identity $Group|Select SID | Export-Csv "c:\SID.csv"
+            }
+             else 
+            {
+                    
+                    get-adgroup -filter *| Export-Csv "c:\ListeGroupe.csv"
             }
 }
 
 elseif ($Serveur -eq "Get-ADGroupMember") 
 {
     $GroupeMember = Read-host "Groupe"
-    Get-ADGroupMember -Identity $GroupeMember |out-file -FilePath "c:\GroupeMember.txt"
+    Get-ADGroupMember -Identity $GroupeMember | Export-Csv "c:\GroupeMember.csv"
 }
 
-else 
+elseif ($Serveur -eq "Get-ADOu") 
 {
-   # $OU = Read-host "unite d organisation : "
-   # Get-ADOrganizationalUnit -Identity $OU |out-file -FilePath "c:\Ou.txt"
+
     Write-host "Taper [1]- Trouver une OU"
     Write-host "Taper [2]- Afficher toutes les OU"
     $OU = Read-Host "Choisissez un numero parmi les 2 propositions"
@@ -71,12 +101,30 @@ else
             If ($OU -eq "Nom")
                 {
                     $OU = Read-host "Nom"
-                    Get-ADOrganizationalUnit -Identity $OU |out-file -FilePath "c:\Ou.txt"
+                    Get-ADOrganizationalUnit -Identity $OU | Export-Csv "c:\Ou.csv"
             }
             else 
             {
                    
-                   Get-ADOrganizationalUnit -Filter 'Name -like "*"' | FT Name, DistinguishedName -A|out-file -FilePath "c:\ListOU.txt"
+                   Get-ADOrganizationalUnit -Filter 'Name -like "*"' | FT Name, DistinguishedName -A| Export-Csv "c:\ListOU.csv"
             }
 }
+
+elseif ($Serveur -eq "Remove-ADUser") 
+{
+   
+    $RemoveUser = Read-host "Pseudo de l'utilisateur à supprimer"
+    Remove-ADUser -Identity $RemoveUser 
+    Write-host "L'utilisateur a été supprimé"
+}
+
+else 
+{
+   
+    $RemoveGroup = Read-host "Groupe à supprimer"
+    Remove-ADGroup -Identity $RemoveGroup
+    Write-host "Le groupe a été supprimé"
+}
+
+
 
