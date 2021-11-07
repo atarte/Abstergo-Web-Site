9ca048189cc9715ee0a7c41b50d76097e3e7b2e0
Lau-cesi <73819470+Lau-cesi@users.noreply.github.com>
2021-11-07 10:16:39 +0100

    ajout droit au groupe admin

--- Scripts/droitsNTFS/GestionDroitNTFS.ps1
+++ Scripts/droitsNTFS/GestionDroitNTFS.ps1
@@ -5,20 +5,21 @@ Foreach ($Line in $AddUserNtfs)
 $User = $Line.user
 
 
-    # Créer un dossier pour chaque utilisateur du CSV
+    # CrÃ©er un dossier pour chaque utilisateur du CSV
     New-Item -ItemType Directory -Path "$PersonnelPathRoot\$User"
 
-    # Desactiver l'heritage tout en copiant les autorisations NTFS héritées
+    # Desactiver l'heritage tout en copiant les autorisations NTFS hÃ©ritÃ©es
     Get-Item "$PersonnelPathRoot\$User" | Disable-NTFSAccessInheritance
 
     # Ajout des autorisations NTFS
     Add-NTFSAccess -Path "$PersonnelPathRoot\$User"  -Account "$User@exia.local" -AccessRights FullControl
+    Add-NTFSAccess -Path "$PersonnelPathRoot\$User"  -Account "GRP_admin@exia.local" -AccessRights FullControl
     
     # Modifier le proprietaire sur le dossier
     Set-NTFSOwner -Path "$PersonnelPathRoot\$User" -Account "$User@exia.local"
     
     # Supprimer des autorisations NTFS
-    Remove-NTFSAccess -Path "$PersonnelPathRoot\$User"  -Account "Utilisateurs authentifiés" -AccessRights FullControl
+    Remove-NTFSAccess -Path "$PersonnelPathRoot\$User"  -Account "Utilisateurs authentifiÃ©s" -AccessRights FullControl
     Remove-NTFSAccess -Path "$PersonnelPathRoot\$User"  -Account "Utilisateurs" -AccessRights FullControl
-    Remove-NTFSAccess -Path "$PersonnelPathRoot\$User"  -Account "Système" -AccessRights FullControl
+    Remove-NTFSAccess -Path "$PersonnelPathRoot\$User"  -Account "SystÃ¨me" -AccessRights FullControl
 } 
