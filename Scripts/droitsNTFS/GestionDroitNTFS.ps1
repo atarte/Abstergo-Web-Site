a3e0924951f550ef374035ebdca662f1ae931b02
Lau-cesi <73819470+Lau-cesi@users.noreply.github.com>
2021-11-07 09:49:38 +0100

    Add files via upload

---
+++ Scripts/droitsNTFS/GestionDroitNTFS.ps1
@@ -0,0 +1,24 @@
+$AddUserNtfs = Import-CSV C:\Users\Administrateur\Documents\UserNtfs.csv -Delimiter "|"
+$PersonnelPathRoot = "C:\employes"
+Foreach ($Line in $AddUserNtfs) 
+{
+$User = $Line.user
+
+
+    # Créer un dossier pour chaque utilisateur du CSV
+    New-Item -ItemType Directory -Path "$PersonnelPathRoot\$User"
+
+    # Desactiver l'heritage tout en copiant les autorisations NTFS héritées
+    Get-Item "$PersonnelPathRoot\$User" | Disable-NTFSAccessInheritance
+
+    # Ajout des autorisations NTFS
+    Add-NTFSAccess -Path "$PersonnelPathRoot\$User"  -Account "$User@exia.local" -AccessRights FullControl
+    
+    # Modifier le proprietaire sur le dossier
+    Set-NTFSOwner -Path "$PersonnelPathRoot\$User" -Account "$User@exia.local"
+    
+    # Supprimer des autorisations NTFS
+    Remove-NTFSAccess -Path "$PersonnelPathRoot\$User"  -Account "Utilisateurs authentifiés" -AccessRights FullControl
+    Remove-NTFSAccess -Path "$PersonnelPathRoot\$User"  -Account "Utilisateurs" -AccessRights FullControl
+    Remove-NTFSAccess -Path "$PersonnelPathRoot\$User"  -Account "Système" -AccessRights FullControl
+} 
